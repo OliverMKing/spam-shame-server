d4544e8e91ce76120abe87fc25a2ca5e4429fb7d
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2020-11-28 20:22:50 -0500

    Add express and barebone routes

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+const express = require("express");
+
+const PORT = 3000;
+const app = express();
+
+app.get("/reports", (req, res) => {
+  // TODO: Return reports
+  res.json({ hello: "World" });
+});
+
+app.post("/reports", (req, res) => {
+  // TODO: Add report to database
+  const { website } = req.body;
+
+  res.json({ reportAdded: true });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
