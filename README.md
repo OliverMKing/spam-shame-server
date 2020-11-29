e538bb86856f318a0959328f600151387834153e
Oliver King <33605867+OliverMKing@users.noreply.github.com>
2020-11-28 21:10:26 -0500

    Add mongoose and functionality

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # spam-shame-server
+
+## Requirements
+Before running this application you have to
+
+1. Make sure you have MongoDB and Node installed
+2. Create a database called `spam_shame` with a collection called `reports`.
+3. Run `npm install` to install all Node.js dependencies
--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+// Configuration options for express
 const PORT = 3000;
 const app = express();
+app.use(bodyParser.json());
 
-app.get("/reports", (req, res) => {
-  // TODO: Return reports
-  res.json({ hello: "World" });
-});
+// Connect to db
+mongoose.connect("mongodb://localhost/spam_shame", { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Database connection error"));
+db.once("open", () => {
+  console.log("Database connected");
 
-app.post("/reports", (req, res) => {
-  // TODO: Add report to database
-  const { website } = req.body;
+  // Define database schema
+  const reportSchema = new mongoose.Schema({ website: String });
+  const Report = mongoose.model("Report", reportSchema);
 
-  res.json({ reportAdded: true });
-});
+  // Specify express (api) routes
+  app.get("/reports", async (req, res) => {
+    // Find all reports and then return them as an array
+    const reports = await Report.find({}).select("website -_id");
+    res.json({ reports: reports.map((x) => x.website) });
+  });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  app.post("/reports", (req, res) => {
+    const { website } = req.body;
+    const newReport = new Report({ website });
+
+    // Add new report to the database
+    newReport.save((err, newReport) => {
+      if (err) {
+        console.error(`Error saving new report for ${website}`);
+        return res.status(500).json({ reportAdded: false });
+      }
+      console.log(`New report saved for ${website}`);
+      res.json({ reportAdded: true });
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
